d12085c795dca875b1b1c333932e5a6b42410127
bassiouny <ahmedbassiouny26@gmail.com>
2018-03-31 16:54:05 +0200

    add fabric and circle image

--- app/src/main/java/bassiouny/ahmed/waslabank/utils/MyApplication.java
+++ app/src/main/java/bassiouny/ahmed/waslabank/utils/MyApplication.java
@@ -5,6 +5,8 @@ import android.app.Application;
 import bassiouny.ahmed.genericmanager.SharedPrefManager;
 import bassiouny.ahmed.waslabank.R;
 import bassiouny.ahmed.waslabank.api.ApiConfig;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by bassiouny on 26/03/18.
@@ -14,6 +16,7 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         SharedPrefManager.init(this,getString(R.string.app_name));
         ApiConfig.initRetrofitConfig();
     }
