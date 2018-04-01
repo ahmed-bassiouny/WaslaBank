5049ff5834acb482c7d49a4f4e526a344f00eb57
bassiouny <ahmedbassiouny26@gmail.com>
2018-04-01 23:20:52 +0200

    add user profile

---
+++ app/src/main/java/bassiouny/ahmed/waslabank/activities/view/UserProfileActivity.java
@@ -0,0 +1,17 @@
+package bassiouny.ahmed.waslabank.activities.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import bassiouny.ahmed.waslabank.R;
+
+// this activity view my profile
+// view profile user use app
+public class UserProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}
