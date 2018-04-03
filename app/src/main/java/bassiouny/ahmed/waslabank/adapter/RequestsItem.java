c08f263ba7829c50a916217236220bb938e8064f
bassiouny <ahmedbassiouny26@gmail.com>
2018-04-04 00:25:03 +0200

    requests list

---
+++ app/src/main/java/bassiouny/ahmed/waslabank/adapter/RequestsItem.java
@@ -0,0 +1,40 @@
+package bassiouny.ahmed.waslabank.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import bassiouny.ahmed.waslabank.R;
+
+/**
+ * Created by bassiouny on 03/04/18.
+ */
+
+public class RequestsItem extends RecyclerView.Adapter<RequestsItem.MyViewHolder> {
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public MyViewHolder(View view) {
+            super(view);
+
+        }
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_request, parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 4;
+    }
+
+}
---
+++ app/src/main/java/bassiouny/ahmed/waslabank/fragments/view/RequestsFragment.java
@@ -0,0 +1,51 @@
+package bassiouny.ahmed.waslabank.fragments.view;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CalendarView;
+
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
+import bassiouny.ahmed.waslabank.R;
+import bassiouny.ahmed.waslabank.adapter.RequestsItem;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RequestsFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    public RequestsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_requests, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        findView(view);
+    }
+
+    private void findView(View view) {
+        recyclerView = view.findViewById(R.id.recycler);
+        // set layout manager
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        // set adapter for recycler view
+        recyclerView.setAdapter(new RequestsItem());
+    }
+}
