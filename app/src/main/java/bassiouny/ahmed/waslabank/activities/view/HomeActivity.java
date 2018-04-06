95b56871a1b0af0ba964ab13ef1239151c54f41e
bassiouny <ahmedbassiouny26@gmail.com>
2018-04-06 15:32:44 +0200

    refactor home menu

--- app/src/main/java/bassiouny/ahmed/waslabank/activities/view/HomeActivity.java
+++ app/src/main/java/bassiouny/ahmed/waslabank/activities/view/HomeActivity.java
@@ -1,41 +1,97 @@
 package bassiouny.ahmed.waslabank.activities.view;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.GridView;
+import android.widget.Toast;
 
 import bassiouny.ahmed.genericmanager.SharedPrefManager;
 import bassiouny.ahmed.waslabank.R;
+import bassiouny.ahmed.waslabank.adapter.HomeMenuItem;
+import bassiouny.ahmed.waslabank.interfaces.ItemClickInterface;
 import bassiouny.ahmed.waslabank.utils.SharedPrefKey;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements ItemClickInterface {
+
+    private RecyclerView recyclerView;
+    private int[] menuImages;
+    private String[] menuStrings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        findView();
+        initObject();
+        onClick();
         findViewById(R.id.iv_profile).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(HomeActivity.this,UserProfileActivity.class));
+                startActivity(new Intent(HomeActivity.this, UserProfileActivity.class));
             }
         });
-        findViewById(R.id.tv_setting).setOnClickListener(new View.OnClickListener() {
+    }
+
+    private void initObject() {
+        // set menu image array
+        menuImages = new int[]{R.drawable.car_plus, R.drawable.car, R.drawable.ic_call, R.drawable.logo_w, R.drawable.ic_call_missed};
+        // set menu string array
+        menuStrings = getResources().getStringArray(R.array.menu);
+        // check image and string array
+        // if not equal throw exception to make app crush
+        if(menuImages.length != menuStrings.length)
+            throw new RuntimeException("Image Array Not Equal String Array");
+        // set item menu in recycler view
+        setHomeMenu();
+    }
+
+    private void onClick() {
+    }
+
+    private void findView() {
+        recyclerView = findViewById(R.id.recycler);
+    }
+
+    /*
+    * this method create menu string and image
+    * create layout manager
+    * create adapter
+    * */
+    private void setHomeMenu() {
+        // create gridlayout manager
+        GridLayoutManager manager = new GridLayoutManager(this, 2);
+        // create span size
+        // if last row is one item make it in middle
+        // if last row is two item make it in normal
+        manager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
             @Override
-            public void onClick(View view) {
-                SharedPrefManager.setObject(SharedPrefKey.USER,"");
-                startActivity(new Intent(HomeActivity.this,SplashActivity.class));
-                finish();
+            public int getSpanSize(int position) {
+                if (menuImages.length % 2 != 0) {
+                    return (position == menuImages.length - 1) ? 2 : 1;
+                } else {
+                    return 1;
+                }
             }
         });
-        findViewById(R.id.iv_setting).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SharedPrefManager.setObject(SharedPrefKey.USER,"");
-                startActivity(new Intent(HomeActivity.this,SplashActivity.class));
-                finish();
-            }
-        });
+        // create adapter
+        HomeMenuItem adapter = new HomeMenuItem(this, menuStrings, menuImages);
+        // set layout manager
+        recyclerView.setLayoutManager(manager);
+        // set adapter
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void getItem(@Nullable Object o, int position) {
+        if(position == 4){
+            SharedPrefManager.setObject(SharedPrefKey.USER,"");
+            startActivity(new Intent(HomeActivity.this,SplashActivity.class));
+            finish();
+        }
     }
 }
--- app/src/main/java/bassiouny/ahmed/waslabank/interfaces/ItemClickInterface.java
+++ app/src/main/java/bassiouny/ahmed/waslabank/interfaces/ItemClickInterface.java
@@ -1,9 +1,12 @@
 package bassiouny.ahmed.waslabank.interfaces;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
 /**
  * Created by bassiouny on 26/03/18.
  */
 
 public interface ItemClickInterface<T> {
-    void getItem(T t,int position);
+    void getItem(@Nullable T t, int position);
 }
