b13fc116882b4592c0dcd6bf3f96447d7db7e29c
bassiouny <ahmedbassiouny26@gmail.com>
2018-04-07 14:10:28 +0200

    add trip details fragment and feedback

---
+++ app/src/main/java/bassiouny/ahmed/waslabank/fragments/view/TripDetailsFragment.java
@@ -0,0 +1,30 @@
+package bassiouny.ahmed.waslabank.fragments.view;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import bassiouny.ahmed.waslabank.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TripDetailsFragment extends Fragment {
+
+
+    public TripDetailsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_trip_details, container, false);
+    }
+
+}
